892b0fd68075ba07051e3c47c98111efce3e9c29
Imre <csullog91@gmail.com>
2016-04-11 20:25:03 +0200

    svasta nesto

--- src/dao/TimDAO.java
+++ src/dao/TimDAO.java
@@ -10,26 +10,29 @@ import entities.Tim;
 
 public class TimDAO {
 	private static EntityManager em = Persistence.createEntityManagerFactory("ProjectX").createEntityManager();
-	
-	public static Tim napraviTim(Tim t){
+
+	public static Tim napraviTim(Tim t) {
 		try {
 			em.getTransaction().begin();
-			em.persist(t);			//on ovde napravi SQL i kreira igraca					
+			em.persist(t); // on ovde napravi SQL i kreira igraca
 			em.getTransaction().commit();
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-		return t;		
+		return t;
 	}
-	public static Tim ispisiTim(int id){
+
+	public static Tim ispisiTim(int id) {
 		return em.find(Tim.class, id);
 	}
-	public static List<Tim> ispisiSveTimove(){
-		Query q = em.createQuery("select t from Tim t");  
+
+	public static List<Tim> ispisiSveTimove() {
+		Query q = em.createQuery("select t from Tim t");
 		return q.getResultList();
 	}
-	public static boolean izbrisiTim(Tim t){
+
+	public static boolean izbrisiTim(Tim t) {
 		try {
 			em.getTransaction().begin();
 			Tim tim = em.merge(t);
@@ -41,18 +44,31 @@ public class TimDAO {
 		}
 		return true;
 	}
-	public static void main(String[] args){
+	
+	public static Tim azurirajTim(Tim t) {
+		try {
+			em.getTransaction().begin();
+			em.merge(t);
+			em.getTransaction().commit();
+			return t;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void main(String[] args) {
 		Tim tim = new Tim();
 		tim.setNazivTima("JavaScript");
-		
+
 		napraviTim(tim);
-		
-		/*for(Tim t:ispisiSveTimove()){
-			System.out.println(t.getNazivTima());
-		}*/
-		/*Tim t = ispisiTim(1);
-		izbrisiTim(t);
-		for(Tim te:ispisiSveTimove()){
-		System.out.println(te.getNazivTima());*/
+
+		/*
+		 * for(Tim t:ispisiSveTimove()){ System.out.println(t.getNazivTima()); }
+		 */
+		/*
+		 * Tim t = ispisiTim(1); izbrisiTim(t); for(Tim te:ispisiSveTimove()){
+		 * System.out.println(te.getNazivTima());
+		 */
 	}
 }
